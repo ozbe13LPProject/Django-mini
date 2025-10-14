efc480d0c79605e52d233468f911c025bd39016b
summmer5 <jisun131200@gmail.com>
2025-10-14 11:21:40 +0900

    urls.py 추가
    # Conflicts:
    #	config/urls.py
    #	pyproject.toml

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_spectacular',
+    'drf_yasg',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
