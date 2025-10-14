e23098a10fd3d801576bf1d7fe96fa16a2c37be4
summmer5 <jisun131200@gmail.com>
2025-10-14 14:46:31 +0900

    urls.py fix

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,3 @@
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework import permissions
 
 from django.contrib import admin
 from django.urls import path
@@ -10,22 +7,8 @@ from drf_spectacular.views import (
     SpectacularSwaggerView
 )
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="API Documentation",
-        default_version="v1",
-        description="API description",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 urlpatterns = [
-path('admin/', admin.site.urls),
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-
+    path('admin/', admin.site.urls),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
