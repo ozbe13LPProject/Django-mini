571c2fefd2c0194a7f8c4ffb654b27c28702d52c
gmansu1 <gmansu177>
2025-10-15 19:15:03 -0700

    .

--- config/urls.py
+++ config/urls.py
@@ -16,31 +16,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
-)
-
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.http import HttpResponse
 
+
 def home(request):
-    return HttpResponse("")
+    return HttpResponse("Welcome to the Bank API")
+
 
 urlpatterns = [
     path('', home),
-    path('accounts/', include('apps.app_name.urls')),
     path('admin/', admin.site.urls),
+
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+
+    path('api/', include('apps.app_name.urls')),
+
+
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include('apps.app_name.urls')),]
+]
+
 
 
