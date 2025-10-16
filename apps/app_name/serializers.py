14217fd5c2a3f38a2b823cd0d4a2856c1bbc0ad4
gmansu1 <gmansu177>
2025-10-15 18:57:46 -0700

    ..

--- apps/app_name/serializers.py
+++ apps/app_name/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
-from .models import Transaction
+from .models import Account, Transaction
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
+        read_only_fields = ['account_id', 'user', 'created_at']
+
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = '__all__'
+        read_only_fields = ['transaction_id', 'transaction_at']
+
+
 
 
--- apps/app_name/services.py
+++ apps/app_name/services.py
@@ -1,21 +1,26 @@
+from django.shortcuts import get_object_or_404
 from . import repositories
 
-def list_user_accounts(user):
-    return repositories.get_accounts_by_user(user)
+
+def list_user_accounts(user, filters=None):
+    return repositories.get_accounts_by_user(user, filters)
+
 
 def create_new_account(user, validated_data):
     return repositories.create_account(
         user=user,
         bank_name=validated_data['bank_name'],
         account_number=validated_data['account_number'],
-        balance=validated_data['balance']
+        balance=validated_data.get('balance', 0),
+        bank_code=validated_data['bank_code'],
     )
 
+
 def delete_account_if_owner(user, account_id):
-    account = repositories.get_account_by_id(account_id)
-    if account is None:
-        return False, "계좌를 찾을 수 없습니다."
+    account = get_object_or_404(repositories.Account, account_id=account_id)
     if account.user != user:
         return False, "삭제 권한이 없습니다."
     repositories.delete_account(account)
     return True, None
+
+
